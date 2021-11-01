a53dbc4800e89bfe7064da03b80a9ff5a0b44367
Chen-techa <17502248584@163.com>
2021-11-01 15:01:40 +0800

    '删除一些不必要的文件，进一步初始化项目结构，完成router的基本配置'

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+        //配置页面路由        
+    ]
+})
+
+export default router
